f87e6849e190fceb563c26cecbfb1552779b555e
nunnallybetsy <35079850+nunnallybetsy@users.noreply.github.com>
2018-02-05 10:31:39 -0800

    template

---
+++ template/header.php
@@ -0,0 +1,20 @@
+ <?php 
+
+    echo '<header>
+    <img id="logo" src="images/logo.png">
+	<h1 id="afterlogo">Multisport Events</h1>
+    <nav>
+      <div href="#" class="hamburger">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+      </div>
+      <ul class="clearfix menu">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Details</a></li>
+        <li><a href="#">Preperation</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  </header>'
+    ?>
