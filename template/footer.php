39ed6c33e64f79c5cfe82219835c68de264a1fde
nunnallybetsy <35079850+nunnallybetsy@users.noreply.github.com>
2018-02-05 10:43:23 -0800

    Delete footer.php

--- template/footer.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-    echo '<footer><p>Ace in the Hole Multisport Events<br>
-    Copyright &copy; BS Designs. All rights reversed.</p>
-	<a class="fa fa-facebook" href="#"></a> <a class="fa fa-twitter" href="#"></a>
-    </footer>'
-    ?>
