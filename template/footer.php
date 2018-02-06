de347d237e3f255e2d392ba750d3b4970768ee84
nunnallybetsy <35079850+nunnallybetsy@users.noreply.github.com>
2018-02-05 21:32:36 -0800

    Finale Template Upload

---
+++ template/footer.php
@@ -0,0 +1,7 @@
+<?php 
+
+    echo '<footer><p>Ace in the Hole Multisport Events<br>
+    Copyright &copy; BS Designs. All rights reversed.</p>
+	<a class="fa fa-facebook" href="#"></a> <a class="fa fa-twitter" href="#"></a>
+    </footer>'
+    ?>
---
+++ template/template.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<meta content="width=device-width, height=device-height, initial-scale=1" name="viewport">
+	<title>Ace in the Hole Multisport Events</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="css/reset.css" rel="stylesheet" type="text/css">
+	<link href="css/helper.css" rel="stylesheet" type="text/css">
+	<link href="css/grid.css" rel="stylesheet" type="text/css">
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<header>
+		<?php include ('header.php');?>
+	</header>
+
+	<main>
+		<img id="heroimg" src="http://via.placeholder.com/350x150">
+
+		<h2>Heading 2</h2>
+
+		<p>
+			Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+		</p>
+
+		<p>
+			Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+		</p>
+
+		<div>
+			<h3>Heading 3</h3>
+			<p>More page content contained inside the div. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+
+		</div>
+
+	</main>
+
+	<footer>
+		<?php include ('footer.php');?>
+	</footer>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="hamburger.js"></script>
+
+</body>
+
+</html>
